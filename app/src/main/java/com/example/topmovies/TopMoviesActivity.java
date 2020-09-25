f4a2b74cd0c333a31b9e0952c134fdae618917fa
mohamadeslami <mohammadeslami1379@yahoo.com>
2020-09-25 14:02:49 +0330

    top movies activity created and designed

---
+++ app/src/main/java/com/example/topmovies/TopMoviesActivity.java
@@ -0,0 +1,14 @@
+package com.example.topmovies;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TopMoviesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_top_movies);
+    }
+}
